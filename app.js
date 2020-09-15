084aa51e71b39f98419c90cc18dc2c976ed8c487
Nizar <niezar.rk@gmail.com>
2020-09-15 21:26:48 +0700

    create router endpoint to artcles and books

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
 const express = require('express');
 const app = express();
+const messages = require('./messages.json');
+const axios = require('axios');
 require('dotenv').config();
 
 
 app.get('/', (req, res) => {
-    console.log(process.env.API_KEY);
-  res.send('Hello World!');
+  res.send(messages.OK);
 });
 
-app.listen(3000, () => {
+app.get('/articles/get?:fq?:sort', async (req, res) => {
+    // use fq to filter such as headline, subject, source etc with standard Lucene syntax
+    // use sort oldest / newer to sort data
+    try {
+        console.log(req.query);
+        let get = await axios.get(`${process.env.API}/search/v2/articlesearch.json?sort=${req.query.sort}&api-key=${process.env.API_KEY}&fq=${req.query.fq}`);
+
+        if (get.data.response.docs.length == 0) {
+            messages.INVALID.message = 'Not Found!';
+            messages.INVALID.data = get.data;
+            res.send(messages.INVALID);
+        } else {
+            messages.OK.data = get.data;
+            res.send(messages.OK);
+        }
+    } catch (error) {
+        console.log(error);
+        messages.ERR.data = error;
+        res.send(messages.ERR);
+    }
+});
+
+app.get('/books/get?:list', async (req, res) => {
+    // use list to search "e-book-fiction" and "hardcover-fiction"
+    try {
+        console.log(req.query);
+        let get = await axios.get(`${process.env.API}/books/v3/lists.json?api-key=${process.env.API_KEY}&list=${req.query.list}`);
+
+        if (get.data.num_results == 0) {
+            messages.INVALID.message = 'Not Found!';
+            messages.INVALID.data = get.data;
+            res.send(messages.INVALID);
+        } else {
+            messages.OK.data = get.data;
+            res.send(messages.OK);
+        }
+    } catch (error) {
+        console.log(error);
+        messages.ERR.data = error;
+        res.send(messages.ERR);
+    }
+});
+
+app.listen(process.env.PORT || 3001, () => {
   console.log('App listening on port 3000!');
 });
